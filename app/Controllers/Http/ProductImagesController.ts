e5ec57c677c88d159ea7e5a7ef65b43e2f640e6b
Mike190061 <mfkhoirurrijal@gmail.com>
2022-05-15 14:45:38 +0700

    added sorting by date

--- app/Controllers/Http/ProductImagesController.ts
+++ app/Controllers/Http/ProductImagesController.ts
@@ -1,55 +1,56 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import ProductImage from 'App/Models/ProductImage'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import ProductImage from "App/Models/ProductImage";
 
 export default class ProductImagesController {
   public async delete({ auth, request, response }: HttpContextContract) {
-    await auth.use('api').authenticate()
+    await auth.use("api").authenticate();
 
-    const productImage = await ProductImage.findBy('id', request.param('id'))
+    const productImage = await ProductImage.findBy("id", request.param("id"));
 
     if (!productImage) {
       return response.notFound({
         status: 404,
-        message: 'Product image not found'
-      })
+        message: "Product image not found",
+      });
     }
 
     try {
-      await productImage.delete()
+      await productImage.delete();
       return response.ok({
         status: 200,
-        message: 'Product image deleted successfully'
-      })
+        message: "Product image deleted successfully",
+      });
     } catch {
       return response.badRequest({
         status: 400,
-        message: 'Product image failed to delete'
-      })
+        message: "Product image failed to delete",
+      });
     }
   }
 
   public async store({ auth, request, response }: HttpContextContract) {
-    await auth.use('api').authenticate()
+    await auth.use("api").authenticate();
 
     try {
-      const images: ProductImage[] = []
-      for (let image of request.input('images')) {
+      const images: ProductImage[] = [];
+      for (let image of request.input("images")) {
         const result = await ProductImage.create({
-          source: image
-        })
-        images.push(result)
+          productId: request.param("productId"),
+          source: image,
+        });
+        images.push(result);
       }
 
       return response.created({
         status: 201,
-        message: 'Product image created successfully',
-        data: images
-      })
+        message: "Product image created successfully",
+        data: images,
+      });
     } catch {
       return response.badRequest({
         status: 400,
-        message: 'Product image failed to create'
-      })
+        message: "Product image failed to create",
+      });
     }
   }
 }
--- app/Middleware/MultipleImageUploader.ts
+++ app/Middleware/MultipleImageUploader.ts
@@ -1,37 +1,41 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { resolve } from 'path'
-import { mkdirSync, existsSync } from 'fs'
-import sharp from 'sharp'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { resolve } from "path";
+import { mkdirSync, existsSync } from "fs";
+import sharp from "sharp";
 
 export default class MultipleImageUploader {
-  public async handle({ request }: HttpContextContract, next: () => Promise<void>, paths: string[]) {
-    const uploadDir = resolve(__dirname, '../../upload/images')
+  public async handle(
+    { request }: HttpContextContract,
+    next: () => Promise<void>,
+    paths: string[]
+  ) {
+    const uploadDir = resolve(__dirname, "../../upload/images");
 
     if (!existsSync(uploadDir)) {
-      mkdirSync(uploadDir, { recursive: true })
+      mkdirSync(uploadDir, { recursive: true });
     }
 
-    const images: string[] = request.input(paths[0])
-    const imagePaths: string[] = []
+    const images: string[] = request.input(paths[0]) || [];
+    const imagePaths: string[] = [];
     for (let image of images) {
-      const base64 = image.split(';base64,')[1]
-      const buffer = Buffer.from(base64, 'base64')
-      const filename = Date.now() + '.webp'
+      const base64 = image.split(";base64,")[1];
+      const buffer = Buffer.from(base64, "base64");
+      const filename = Date.now() + ".webp";
 
       await sharp(buffer)
         .webp({
-          quality: 80
+          quality: 80,
         })
-        .toFile(uploadDir + '/' + filename)
+        .toFile(uploadDir + "/" + filename);
 
-      imagePaths.push('images/' + filename)
+      imagePaths.push("images/" + filename);
     }
 
     request.updateBody({
       ...request.body(),
-      [paths[0]]: imagePaths
-    })
+      [paths[0]]: imagePaths,
+    });
 
-    await next()
+    await next();
   }
 }
